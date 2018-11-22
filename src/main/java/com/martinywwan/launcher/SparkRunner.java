107951526b0edfd8afc721a93e23c09955b595a1
martinywwan <martin.yw.wan@gmail.com>
2018-11-22 00:24:06 +0000

    added pom file and log4j properties

--- src/main/java/com/martinywwan/launcher/SparkRunner.java
+++ src/main/java/com/martinywwan/launcher/SparkRunner.java
@@ -1,6 +1,5 @@
 package com.martinywwan.launcher;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -16,30 +15,32 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-@Slf4j
 @Component
 public class SparkRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Collection<String> topics = Arrays.asList("example");
+        String topic1 = "martin";
+        String topic2 = "test";
         SparkConf sparkConf = new SparkConf().setAppName("martinywwan app").setMaster("local[*]");
-        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
-
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10));
         Map<String, Object> kafkaParams = new HashMap<>();
         kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, "testId2");
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, "testId6");
         kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(jssc, LocationStrategies.PreferConsistent(),
-                ConsumerStrategies.Subscribe(topics, kafkaParams)); //StreamingContext object has to be created which is the main entry point of all Spark Streaming functionality.
-        stream.map(x -> x.value()).print();
+                ConsumerStrategies.Subscribe(Arrays.asList(topic1), kafkaParams)); //StreamingContext object has to be created which is the main entry point of all Spark Streaming functionality.
+        stream.map(x -> topic1+"--"+x.value()).print();
+
+        JavaInputDStream<ConsumerRecord<String, String>> stream2 = KafkaUtils.createDirectStream(jssc, LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(Arrays.asList(topic2), kafkaParams)); //StreamingContext object has to be created which is the main entry point of all Spark Streaming functionality.
+        stream2.map(x -> topic2+"--"+x.value()).print();
 
         jssc.start();
         jssc.awaitTermination();
